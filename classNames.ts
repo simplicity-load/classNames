8860167897500d43a21a2dda57db5f7bf8ffebef
my <my@mail.com>
2024-11-02 13:56:56 +0100

    feat: add classnames

---
+++ classNames.ts
@@ -0,0 +1,19 @@
+import { filterPredicatedValues, isPredicateMap } from "./filterPredicatedValues";
+
+type Opt = Record<string, boolean>;
+
+const isLastIndex = (i: number, length: number) => i + 1 === length;
+
+export const classNames = (...args: (string | Opt)[]): string => {
+  const classnames = args.reduce<string[]>((acc, arg) => {
+    if (typeof arg === "string") return acc.concat(arg);
+    if (isPredicateMap(arg))
+      return acc.concat(filterPredicatedValues(arg).map(String));
+    throw new Error("Arguments must be either string or boolean map!");
+  }, []);
+  return classnames.reduce(
+    (acc, classname, i, { length }) =>
+      acc.concat(classname).concat(isLastIndex(i, length) ? "" : " "),
+    ""
+  );
+};
---
+++ filterPredicatedValues.ts
@@ -0,0 +1,24 @@
+export const filterPredicatedValues = <T>(
+  x: | [T, boolean][]
+    // @ts-expect-error
+    | Record<T, boolean>
+): T[] => {
+  if (isPredicateMap(x))
+    return filterPredicatedValues(predicateMapToArr(x) as [T, boolean][]);
+  return x.filter(([, predicate]) => predicate).map(([value]) => value);
+};
+
+type MapValues = string | number | symbol;
+export const isPredicateMap = <T extends MapValues>(
+  x: any
+): x is Record<T, boolean> => {
+  if (!x || typeof x !== "object") return false;
+  return Object.entries(x).every(
+    ([k, v]) =>
+      ["string", "number", "symbol"].includes(typeof k) &&
+      typeof v === "boolean"
+  );
+};
+
+const predicateMapToArr = <T extends MapValues>(x: Record<T, boolean>) =>
+  Object.entries(x) as [T, boolean][];
